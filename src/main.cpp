70c364558e994246235ea5f0da0b89b0d27b23f0
Jesse Haynie <jessehaynie02@gmail.com>
2024-08-23 16:31:11 -0600

    changed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,34 @@
 #include "ChessGame.hpp"
 
 int main(int argc, char* argv[]) {
-    initializeSDL();
-    // code to test code coverage
-    ifstream ifs;
-    string curGame;
-    regex patternOne(R"(\d+\.\s*)");
-    ifs.open("testChessGame/testGames.txt", ifstream::in);
+    // initializeSDL();
+    // // code to test code coverage
+    // ifstream ifs;
+    // string curGame;
+    // regex patternOne(R"(\d+\.\s*)");
+    // ifs.open("testChessGame/testGames.txt", ifstream::in);
 
-    while (getline(ifs, curGame)) {
-        if (curGame[0] == '1') {
-            // cout << curGame << endl;
-            if (curGame.find("forfeits") != string::npos || curGame.find("resigns") != string::npos ||
-                curGame.find("time") != string::npos || curGame.find("agreement") != string::npos || 
-                curGame.find("adjudication") != string::npos) {
-                // cout << "skipping game:\n" << curGame << endl;
-                curGame.clear();
-                continue;
-            }
-            curGame = regex_replace(curGame, patternOne, "");
-            // cout << curGame << endl;
-            istringstream curGameStream(curGame);
-            playChess(curGameStream);
-        }
-    }
+    // while (getline(ifs, curGame)) {
+    //     if (curGame[0] == '1') {
+    //         // cout << curGame << endl;
+    //         if (curGame.find("forfeits") != string::npos || curGame.find("resigns") != string::npos ||
+    //             curGame.find("time") != string::npos || curGame.find("agreement") != string::npos || 
+    //             curGame.find("adjudication") != string::npos) {
+    //             // cout << "skipping game:\n" << curGame << endl;
+    //             curGame.clear();
+    //             continue;
+    //         }
+    //         curGame = regex_replace(curGame, patternOne, "");
+    //         // cout << curGame << endl;
+    //         istringstream curGameStream(curGame);
+    //         playChess(curGameStream);
+    //     }
+    // }
 
-    ifs.close();
+    // ifs.close();
 
     // actual program
     // cout << "Playing chess" << endl;
-    // playChess(cin);
+    playChess(cin);
     return 0;
 }
